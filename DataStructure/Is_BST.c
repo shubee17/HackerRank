4673199627f526f4b4914ad6483d40efb858f46f
Shubham Dilip Morankar <35920863+shubee17@users.noreply.github.com>
2018-12-07 23:45:47 +0530

    Add files via upload

---
+++ DataStructure/Is_BST.c
@@ -0,0 +1,14 @@
+bool checkBST(Node* root)
+{
+    static struct Node* prev = NULL;
+    if(root)
+    {
+        if(!checkBST(root->left))
+            return false;
+        if(prev != NULL && root->data <= prev->data)
+            return false;
+        prev = root;
+        return checkBST(root->right);
+    }
+    return true;
+}
